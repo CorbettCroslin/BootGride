f2ef02af94047fb1d80278272d00d130575e86c5
Corbett <corbett.croslin@gmail.com>
2021-10-26 18:18:42 -0400

    update

---
+++ BasicJavascript/Exercise1.js
@@ -0,0 +1,8 @@
+alert("Hello World");
+
+var myVaribleCorbett;
+
+myVaribleCorbett= 10;
+
+var myVar2="Hello";
+
---
+++ JSlessions/index.js
@@ -0,0 +1,7 @@
+var personAge = 5;
+
+while (percentage < 10){
+    percentage++;
+    console.log{"You're less than 10"}
+}
+document.write("You're more than 10")
